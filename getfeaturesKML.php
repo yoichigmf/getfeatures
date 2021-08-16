57b93fc0dad627af00878c5ab793eb10179d7d0c
yoichigmf <yoichi.kayama@gmail.com>
2021-08-16 17:53:06 +0900

    debug

--- getfeaturesKML.php
+++ getfeaturesKML.php
@@ -74,6 +74,10 @@ $isdone = false;
 $kml_hd = array('<?xml version="1.0" encoding="UTF-8"?>');
 $kml_hd[] = '<kml xmlns="http://earth.google.com/kml/2.2">';
 $kml_hd[] = ' <Document>';
+$kml_hd[] = ' <name>${sheetname}</name>';
+$kml_hd[] =  '<description/>';
+
+
 
 /*
 $kml_hd[] = ' <Style id="restaurantStyle">';
@@ -111,199 +115,33 @@ $non_locr = array();    //  arrray of non location data for a user
 
 foreach ($sheetd as $index => $cols) {
 
-//echo "\nindex ${index}  ";  //////
-
+echo "\nindex ${index}  ";  //////
+echo "\ndate ${dated}  ";  //////
      $dated = $cols[0];
-     $userd = $cols[1];
+     $timed = $cols[1];
 
-     $kind = $cols[2];
-     $url  = $cols[3];
+     $dist = $cols[2];
+     $addr = $cols[3];
+     $descript = $cols[4];
 
-     $stext = $cols[4];
+     $user = $cols[5];
+     $isource = $cols[6];
+
+     $purl = $cols[7];
+
+     $lot = $cols[8];
+     $lat = $cols[8];
+
+ 
      
-     $client_name = $cols[7];
-     
-     
-$log->addWarning("client_name = ${client_name}\n");
- 
- ###  add 20210712  native client data
- if ( strcmp( $client_name ,'reportpost') ==0 ) {
- 
-        $xcod = (double)$cols[6];    //  coordinate
-        $ycod = (double)$cols[5];
-         $log->addWarning("reportpost\n");
-         
-         
-           if (array_key_exists( $userd, $uid_ar)){   //  is the user id in the array ?
-
-            $ckey = $uid_ar[$userd] + 1;
-           $uid_ar[$userd] = $ckey;
-               }
-        else   {
-           $ckey = 0;
-          $uid_ar[$userd] = $ckey;
-
-            //$non_loc_ar[$userd] = array();
-            }
-
-       $arkey = $userd . "_" . $ckey ;
-       
-       
-              $atrar = array();
-              
-              
-                   $atrar = array();
-         
-                      $atrdata = array(
-                       'date'=> $dated,
-                       'user' => $userd,
-                       'kind' => $kind,
-                       'text' => $stext,
-                       'url'=> $url
-                     );
-                     
-              
-              array_push(  $atrar , $atrdata );
-              
-
-              //             $log->addWarning("feature id == ${arkey}  user == ${userd}");
-         $feature = array(
-           'id' => $arkey,
-           'type' => 'Feature',
-           'geometry' => array(
-           'type' => 'Point',
-       # Pass Longitude and Latitude Columns here
-             'coordinates' => array((double)$xcod, (double)$ycod)
-              ),
-   # Pass other attribute columns here
-           'properties' => array(
-              'user' => $userd,
-              'date' => $dated,
-              'kind' => $kind,
-              'text' => $stext,
-              'url' => $url,
-       'proplist' => $atrar
-       )
-   );
-
-        array_push($geojson['features'], $feature);
-
-    continue;
- }
- 
-
-
- if ( strcmp( $kind ,'location' ) == 0 ) {   //  if record is location data
- 
-          $log->addWarning("line\n");
-
-   //  echo "\nkind ${kind}  ";  sample
-
-
-
-        $xcod = (double)$cols[6];    //  coordinate
-        $ycod = (double)$cols[5];
-
-        if (array_key_exists( $userd, $uid_ar)){   //  is the user id in the array ?
-
-            $ckey = $uid_ar[$userd] + 1;
-            $uid_ar[$userd] = $ckey;
-               }
-        else   {
-            $ckey = 0;
-            $uid_ar[$userd] = $ckey;
-
-            //$non_loc_ar[$userd] = array();
-            }
-
-         $arkey = $userd . "_" . $ckey ;
-
-    
-           $atrar = array();
-           
-
-              //             $log->addWarning("feature id == ${arkey}  user == ${userd}");
-         $feature = array(
-           'id' => $arkey,
-           'type' => 'Feature',
-           'geometry' => array(
-           'type' => 'Point',
-       # Pass Longitude and Latitude Columns here
-             'coordinates' => array((double)$xcod, (double)$ycod)
-              ),
-   # Pass other attribute columns here
-           'properties' => array(
-              'user' => $userd,
-              'date' => $dated,
-              'kind' => $kind,
-              'text' => $stext,
-              'url' => $url,
-       'proplist' => $atrar
-       )
-   );
-
-         array_push($geojson['features'], $feature);
-
-       }    // location
-       else  {
-
-
-
-       if ( $index > 0 ){
-
-
-           if (array_key_exists( $userd, $uid_ar)){
-
-
-                   $ukeyd = $uid_ar[$userd];
-                   $ukey = $userd . "_" . $ukeyd ;
-                   //$arkey = $ukey;
-                  }
-            else  {
-
-
-                  //  $output_ar[$arkey]['attribute'] = array();
-
-                     $ukey = $arkey;
-
-                  }
-                  $attr = array();
-
-   
-
-                     $atrdata = array(
-                       'date'=> $dated,
-                       'user' => $userd,
-                       'kind' => $kind,
-                       'text' => $stext,
-                       'url'=> $url
-                     );
-
-                     $log->addWarning("attribute add  ${ukey}");
-                     foreach ( $geojson['features'] as &$feat){
-
-                          $fkey = $feat["id"];
-
-                        //   $log->addWarning("fkey == ${fkey}");
-
-                           if ( $feat["id"] === $ukey ){
-                             $log->addWarning("add attribute success ============== ${ukey}");
-
-                              array_push(  $feat["properties"]["proplist"], $atrdata );
-                           }
-
-                     }
-
-          }
-       }
-
+  
      }  //  foreach
 
  
      $kmlOutput = join("\n", $kml_hd);
 
 
-     header('Content-type: application/vnd.google-earth.kml+xml');
+     //header('Content-type: application/vnd.google-earth.kml+xml');
      echo $kmlOutput;
 
      $fp = fopen('styles/style_mappin.xml','r');
